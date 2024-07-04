b1904d21c8008550c87acbe26abe2c6014b893b6
trevzhang <trevzhang@163.com>
2024-07-04 22:28:16 +0800

    :zap: Add git_release.sh

--- sh/git_release.sh
+++ sh/git_release.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
-# 获取用户输入的目标分支名称
-read -p "请输入目标分支名称: " release_branch
+# 初始化变量
+release_branch=""
+merge_option="--no-ff"  # 默认使用 --no-ff
+
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --ff)
+            merge_option="--ff"
+            shift
+            ;;
+        --no-ff)
+            merge_option="--no-ff"
+            shift
+            ;;
+        *)
+            release_branch=$1
+            shift
+            ;;
+    esac
+done
+
+# 如果没有提供目标分支，则提示用户输入
+if [ -z "$release_branch" ]; then
+    read -p "请输入目标分支名称: " release_branch
+fi
 
 # 获取当前分支名称
 current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# 显示操作信息
+echo "当前分支: $current_branch"
+echo "目标分支: $release_branch"
+echo "合并选项: $merge_option"
+echo "开始执行合并操作..."
 
 # 保存当前工作区的修改，包括未跟踪的文件
 git stash push --include-untracked
@@ -16,7 +46,7 @@ git checkout $release_branch
 git pull
 
 # 合并当前分支到目标分支
-git merge $current_branch --no-ff
+git merge $current_branch $merge_option
 
 # 推送合并后的目标分支到远程仓库
 git push
